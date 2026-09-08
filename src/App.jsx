1e23ee5e1f00627ccdcb00ac488bc45575a5c490
01-Yang <1785867792@qq.com>
2026-09-08 11:56:10 +0800

    feat: add homepage mentors

--- src/App.jsx
+++ src/App.jsx
@@ -357,6 +357,11 @@ export default function App() {
                   我们相信，成长不必一蹴而就。每天多理解一点、多实践一步、多进步一点，这便是“<strong>日新</strong>”的意义。
                 </p>
                 <p>零一日新社成立于 2026 年 9 月 1 日，由 <strong>零一扬</strong> 担任社长。</p>
+                <div className="home-mentor">
+                  <h3 className="home-trust-subtitle">Mentor</h3>
+                  <p>吴运兵（副教授，现任计算机科学系副主任）</p>
+                  <p>刘洋（某省政府数据智囊团成员、曾就职于中科院数据部门、资深AI政务研讨学者）</p>
+                </div>
                 </div>
 
                 <div className="home-trust-cards">
